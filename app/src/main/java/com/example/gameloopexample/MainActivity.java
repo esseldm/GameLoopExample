388a5e09b57f6174572af47890b79d8fa9010ec3
jkost96 <John.kost@aol.com>
2015-03-30 13:45:46 -0400

    Not really sure whats going on...

--- app/src/main/java/com/example/gameloopexample/MainActivity.java
+++ app/src/main/java/com/example/gameloopexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gameloopexample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,6 +19,7 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, GamePlay.class));
         //Test Test
     }
 
