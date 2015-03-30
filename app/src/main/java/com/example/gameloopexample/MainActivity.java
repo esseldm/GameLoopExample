1fbe3cc8325b7229ea4f7d0fe115b6d03a05339c
jkost96 <John.kost@aol.com>
2015-03-30 13:40:13 -0400

    Test

--- app/src/main/java/com/example/gameloopexample/MainActivity.java
+++ app/src/main/java/com/example/gameloopexample/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        //Test Test
     }
 
 
