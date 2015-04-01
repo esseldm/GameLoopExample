f0f2c8e64fc5563937e6a5a6b13aeb622d4dd901
seleprw1320 <seleprw@hiram.edu>
2015-04-01 12:48:38 -0400

    I pushed my balls in the class they have a velocity and position.

---
+++ app/src/main/java/com/example/gameloopexample/Ball.java
@@ -0,0 +1,12 @@
+package com.example.gameloopexample;
+
+/**
+ * Created by richardselep on 4/1/15.
+ */
+public class Ball {
+
+    public int positionX;
+    public int positionY;
+    public int xVel;
+    public int yVel;
+}
