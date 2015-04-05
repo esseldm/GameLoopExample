756df4bc2bd0a8942cd4798946b1d7b04c9e195c
seleprw1320 <seleprw@hiram.edu>
2015-04-04 22:29:23 -0400

    BAlls

--- app/src/main/java/com/example/gameloopexample/AdvanceBreakout.java
+++ app/src/main/java/com/example/gameloopexample/AdvanceBreakout.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 
 public class AdvanceBreakout extends ActionBarActivity {
@@ -11,6 +12,8 @@ public class AdvanceBreakout extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_advance_breakout);
     }
 
--- app/src/main/java/com/example/gameloopexample/MainActivity.java
+++ app/src/main/java/com/example/gameloopexample/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.gameloopexample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -13,9 +16,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.startGame);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startGame = new Intent(MainActivity.this,AdvanceBreakout.class);
+                startActivityForResult(startGame, 1);
+            }
+        });
     }
 
     @Override
